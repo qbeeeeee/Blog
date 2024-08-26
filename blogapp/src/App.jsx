bd42b9780b50aaf95474fdd10071ab4c43690078
qbeeeeee <foxfireninetails9@gmail.com>
2024-08-26 12:50:26 +0300

    first commit

---
+++ blogapp/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
+import Header from './Components/Header'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
